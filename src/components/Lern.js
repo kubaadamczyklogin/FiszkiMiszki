5fdabe0e46235fd95cb2a8be085915885076fd55
Kuba Adamczyk <poczta@kubaadamczyk.pl>
2022-12-09 22:48:51 +0000

    repair demo version

--- src/components/Lern.js
+++ src/components/Lern.js
@@ -114,7 +114,6 @@ export default function Lern(props) {
     };
 
     const progressData = {
-      lastRepeat: props.testToday,
       lastRepeatData: newlastRepeatData,
       cards: progressDataCards,
     };
